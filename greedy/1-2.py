5c3d8b3cf0e03896d8a60e74c97ec7a0b0f394b3
repl.it user <replituser@example.com>
2021-02-06 16:46:44 +0000

    그리디-큰 수의 법칙

---
+++ greedy/1-2.py
@@ -0,0 +1,16 @@
+n, m, k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+cnt = int(m/(k+1))*k
+cnt += m%(k+1)
+
+result = 0
+result += cnt*first
+result += (m-cnt)*second
+
+print(result)
