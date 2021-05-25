773eea4f0259854868bd4f10e552eb948408ee57
Junhyuk Lim <67899069+jhLim97@users.noreply.github.com>
2021-05-25 18:19:01 +0900

    개미 전사

--- 다이나믹-프로그래밍/2-1.py
+++ 다이나믹-프로그래밍/2-1.py
@@ -1,3 +1,12 @@
-feat: 구현
-refactor: 수정
-test: 테스트 코드 추가
+n = int(input())
+data = list(map(int, input().split()))
+
+d = [0] * 100
+
+d[0] = data[0]
+d[1] = max(data[0], data[1])
+
+for i in range(2, n):
+  d[i] = max(d[i - 1], d[i - 2] + data[i])
+
+print(d[n-1])
