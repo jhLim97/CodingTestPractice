15315828e67357e4eccc3eb9afbcdb702315a533
Junhyuk Lim <67899069+jhLim97@users.noreply.github.com>
2021-07-01 15:36:10 +0900

    네트워크
    프로그래머스 고득점 kit 두가지 풀이

--- DFS,BFS/8-1.py
+++ DFS,BFS/8-1.py
@@ -1,3 +1,4 @@
+#방법 1
 def dfs(visited, computers, x):
     if visited[x]:
         return
@@ -15,4 +16,32 @@ def solution(n, computers):
             dfs(visited, computers, i)
             answer += 1
 
+    return answer
+
+#방법 2
+from collections import deque
+
+def bfs(graph, i, visited):
+    q = deque([i])
+    while q:
+        vertex = q.popleft()
+        visited[vertex] = True
+        for g_ in graph[vertex]:
+            if not visited[g_]:
+                q.append(g_)
+
+def solution(n, computers):
+    answer = 0
+    
+    graph = [[] for i in range(n + 1)]
+    for i, computer in enumerate(computers):
+        for j in range(n):
+            if computer[j] == 1 and i != j:
+                graph[i + 1].append(j + 1)
+    
+    visited = [False] * (n + 1)
+    for i in range(1, n + 1):
+        if not visited[i]:
+            bfs(graph, i, visited)
+            answer += 1
     return answer
