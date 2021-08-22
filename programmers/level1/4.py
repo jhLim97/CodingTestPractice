360a7fde247952ae11ab23570ec292ec980f20ea
Junhyuk Lim <67899069+jhLim97@users.noreply.github.com>
2021-08-22 17:20:15 +0900

    소수 찾기
    1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수

---
+++ programmers/level1/4.py
@@ -0,0 +1,12 @@
+def prime_list(n):
+    sieve = [True] * n
+
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:          
+            for j in range(i+i, n, i):
+                sieve[j] = False
+    return [i for i in range(2, n) if sieve[i] == True]
+def solution(n):
+    answer = len(prime_list(n + 1))
+    return answer
