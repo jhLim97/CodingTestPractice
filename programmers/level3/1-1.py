23a55738460e71f995d12555cf7e617da3579bb0
Junhyuk Lim <67899069+jhLim97@users.noreply.github.com>
2021-09-12 17:47:01 +0900

    네트워크
    컴퓨터의 개수 n, 연결에 대한 정보가 담긴 2차원 배열 computers가 매개변수로 주어질 때, 네트워크의 개수를 반환한다.

---
+++ programmers/level3/1-1.py
@@ -0,0 +1,18 @@
+def dfs(visited, computers, i):
+    if visited[i]:
+        return
+    visited[i] = True
+    for index, computer_ in enumerate(computers[i]):
+        if computer_:
+            dfs(visited, computers, index)
+    
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    
+    for i in range(len(computers)):
+        if not visited[i]:
+            dfs(visited, computers, i)
+            answer += 1
+
+    return answer
